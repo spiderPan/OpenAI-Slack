4d72f6a70f550c9dde298ff85f464d7186fc87a8
Yuliia Karakai <93935356+karakaiyuliia@users.noreply.github.com>
2022-12-14 12:15:11 -0500

    removed commands (#3)
    * removed commands
    
    * open ai added
    
    Co-authored-by: heemalmorakhia11 <heemal.morakhia@wattpad.com>

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { App } = require('@slack/bolt');
+const reqOpenAI = require('./openai/index')
 require("dotenv").config();
 
 const app = new App({
@@ -8,73 +9,16 @@ const app = new App({
   appToken: process.env.SLACK_APP_TOKEN
 });
 
-/* Add functionality here */
-
-app.command("/explain", async ({ command, ack, say }) => {
+app.event("message", async ({ event, say }) => {
   try {
-    await ack();
-    say("Please provide your code");
-    
-    app.event("message", async ({ event, say }) => {
-      try {
-        //event.text is what we need
-        console.log(event);
-        say("Yaaay! that command works!");
-      } catch (error) {
-        console.log("err")
-        console.error(error);
-      }
-    });
-    
+    answer = await reqOpenAI(event.text)
+    say(answer);
   } catch (error) {
     console.log("err")
     console.error(error);
   }
 });
 
-app.command("/translate-code", async ({ command, ack, say }) => {
-  try {
-    await ack();
-    say("PLease provide your code and comments to specify the source and target languages");
-
-    app.event("message", async ({ event, say }) => {
-      try {
-        //event.text is what we need
-        console.log(event);
-        say("Yaaay! that command works!");
-      } catch (error) {
-        console.log("err")
-        console.error(error);
-      }
-    });
-    
-  } catch (error) {
-    console.log("err")
-    console.error(error);
-  }
-});
-
-app.command("/time-complexity", async ({ command, ack, say }) => {
-  try {
-    await ack();
-    say("Please provide your code to calculate time complexity");
-
-    app.event("message", async ({ event, say }) => {
-      try {
-        //event.text is what we need
-        console.log(event);
-        say("Yaaay! that command works!");
-      } catch (error) {
-        console.log("err")
-        console.error(error);
-      }
-    });
-    
-  } catch (error) {
-    console.log("err")
-    console.error(error);
-  }
-});
 
 (async () => {
   // Start the app
--- openai/index.js
+++ openai/index.js
@@ -1,13 +1,33 @@
-//TODO: Use the following as a template for the OpenAI API wrapper
 const { Configuration, OpenAIApi } = require("openai");
 
+require("dotenv").config();
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-const completion = await openai.createCompletion({
-  model: "text-davinci-002",
-  prompt: "Hello world",
-});
-console.log(completion.data.choices[0].text);
+module.exports = async function reqOpenAI(input) {
+  const explainCompletion = await openai.createCompletion({
+    model: "code-davinci-002",
+    prompt: input + "\n\"\"\"\nHere is what the above code snippet is doing:\n1.",
+    temperature: 0,
+    max_tokens: 64,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+    stop: ["\"\"\""]
+  });
+
+  const tcCompletion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: input + "\n\"\"\"\nThe time complexity of the above code snippet is",
+    temperature: 0,
+    max_tokens: 64,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+    stop: ["\"\"\""]
+  });
+
+  return "*Explanation:*\n1." + explainCompletion.data.choices[0].text + "\n\n*Time Complexity:*\nThe time complexity of the above code snippet is" + tcCompletion.data.choices[0].text + "\n"
+}
