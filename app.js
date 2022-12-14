3299e944a5b402876874c815b4913ccbf8a6aad3
Yuliia Karakai <yuliia.karakai@wattpad.com>
2022-12-14 11:11:57 -0500

    removed commands

--- app.js
+++ app.js
@@ -9,72 +9,17 @@ const app = new App({
 });
 
 /* Add functionality here */
-
-app.command("/explain", async ({ command, ack, say }) => {
+app.event("message", async ({ event, say }) => {
   try {
-    await ack();
-    say("Please provide your code");
-    
-    app.event("message", async ({ event, say }) => {
-      try {
-        //event.text is what we need
-        console.log(event);
-        say("Yaaay! that command works!");
-      } catch (error) {
-        console.log("err")
-        console.error(error);
-      }
-    });
-    
+    //event.text is what we need
+    console.log(event);
+    say("Yaaay! that command works!");
   } catch (error) {
     console.log("err")
     console.error(error);
   }
 });
 
-app.command("/translate-code", async ({ command, ack, say }) => {
-  try {
-    await ack();
-    say("PLease provide your code and comments to specify the source and target languages");
-
-    app.event("message", async ({ event, say }) => {
-      try {
-        //event.text is what we need
-        console.log(event);
-        say("Yaaay! that command works!");
-      } catch (error) {
-        console.log("err")
-        console.error(error);
-      }
-    });
-    
-  } catch (error) {
-    console.log("err")
-    console.error(error);
-  }
-});
-
-app.command("/time-complexity", async ({ command, ack, say }) => {
-  try {
-    await ack();
-    say("Please provide your code to calculate time complexity");
-
-    app.event("message", async ({ event, say }) => {
-      try {
-        //event.text is what we need
-        console.log(event);
-        say("Yaaay! that command works!");
-      } catch (error) {
-        console.log("err")
-        console.error(error);
-      }
-    });
-    
-  } catch (error) {
-    console.log("err")
-    console.error(error);
-  }
-});
 
 (async () => {
   // Start the app
